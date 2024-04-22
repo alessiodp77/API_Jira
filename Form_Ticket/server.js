1f0dab4f0c9c56a6ceeb5f17002f03beb113e4f0
alessiodp77 <149113031+alessiodp77@users.noreply.github.com>
2024-04-22 15:55:40 +0200

    pulizia console.log

--- Form_Ticket/server.js
+++ Form_Ticket/server.js
@@ -5,15 +5,11 @@ const axios = require('axios');
 const PORT = process.env.PORT || 3000;
 //const ENVIRONMENT = process.env.url;
 const server = http.createServer((req, res) => {
-    //console.log("ENVIRONMENT: " + ENVIRONMENT);
-    console.log("step1");
-    console.log("req.method:" + req.method);
     if (req.method === 'POST' && req.url === '/submit') {
         let body = '';
         req.on('data', (chunk) => {
             body += chunk.toString();
         });
-        console.log("body:" + body);
         req.on('end', () => {
             const data = JSON.parse(body);
             const nome = data.name;
@@ -49,26 +45,20 @@ const server = http.createServer((req, res) => {
             //res.end('Dati ricevuti con successo!');
         });
     } else if (req.method === 'GET' && req.url === '/CreaTicket') {
-        console.log("step3");
-        // Servi la pagina CreaTicket.html
+        // Servo la pagina CreaTicket.html
         fs.readFile(path.join(__dirname, 'CreaTicket.html'), (err, data) => {
             if (err) {
-                console.log("step4")
-                console.log("__dirname-->" + path.join(__dirname, 'CreaTicket.html'));
                 res.writeHead(404);
                 res.end("File not found!!!!!");
                 return;
             }
-            console.log("path.join(__dirname, 'CreaTicket.html':" + path.join(__dirname, 'CreaTicket.html'));
-            console.log("step3bis");
             res.writeHead(200, {'Content-Type': 'text/html'});
             //console.log("data:" + data);
             res.write(data);
             res.end();
         });
     } else {
-        console.log("step5")
-        // Servi la pagina principale
+        // Servo la pagina principale
         fs.readFile(path.join(__dirname, 'CreaTicket.html'), (err, data) => {
             if (err) {
                 res.writeHead(404);
